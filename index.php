b08d613a7419a857a9259ddeb21505d68bb8bd14
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-09-14 12:10:09 -0300

    style - estilização do header da página de apresentação

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
+<?php?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/style.css">
     <title>Página de apresentação</title>
 </head>
 <body>
     <header class="hd_apresentacao">
-        <div>
-            <h1>RAILLIFE</h1>
-        </div>
-        <nav>
-            <a href="#">Login</a>
-            <a href="#">Cadastro</a>
-        </nav>
+            <div>
+                <img src="assets/logo.png" alt="" class="img_apresentacao">
+            </div>
+            <nav class="nav_apresentacao">
+                <a href="#">Login</a>
+                <a href="#">Cadastro</a>
+            </nav>
     </header>
 </body>
 </html>
