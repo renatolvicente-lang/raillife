e37af31a72ce04ab21ff37c49086f45796ecf4a5
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-09-14 11:31:48 -0300

    up - conclusão do html do header da página de apresentação

--- index.php
+++ index.php
@@ -4,19 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/style.css">
-
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Página de apresentação</title>
 </head>
 <body>
-    <header>
+    <header class="hd_apresentacao">
         <div>
-
+            <h1>RAILLIFE</h1>
         </div>
         <nav>
-            <a href=""></a>
-            <a href=""></a>
+            <a href="#">Login</a>
+            <a href="#">Cadastro</a>
         </nav>
-
     </header>
 </body>
 </html>
