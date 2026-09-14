10c6d572615b662d8cbfde5aae9df00bf8e3d981
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-09-14 12:20:26 -0300

    add - criação da conexão com o banco de dados

---
+++ infra/conn.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "root";
+$database = "raillife";
+$conn = new mysqli($host, $user, $password, $database);
+
+if(!$conn){
+    die("Falha na conexão: ". mysli_connect_error());
+}
+
+
+?>
