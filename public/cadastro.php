f6a1148517ffdfcdac85783a58baf1ea3921e6b6
RoederArt <arthur_roeder@estudante.sesisenai.org.br>
2026-09-14 10:04:09 -0300

    up - estruturação de cadastro

--- public/cadastro.php
+++ public/cadastro.php
@@ -7,7 +7,13 @@
 </head>
 <body>
 
-<header></header>
+<header>
+    <nav>
+        <a href="">Login</a>
+        <a href="">Cadastro</a>
+        <a href="">Panorama geral</a>
+    </nav>
+</header>
     
 <main>
     <div>
@@ -33,9 +39,25 @@
             <input type="email" placeholder="Digite a área em que atua">
             <br>
             <br>
-            <label for="">E-mail:</label>
+            <label for="">Endereço:</label>
             <br>
-            <input type="email" placeholder="Digite seu E-mail"> 
+            <input type="text" placeholder="Digite seu endereço">
+            <br>
+            <br>
+            <label for="">Cidade:</label>
+            <br>
+            <input type="text" placeholder="Digite sua cidade">
+            <br>
+            <br>
+            <label for="">CEP:</label>
+            <br>
+            <input type="number" placeholder="Digite seu Código de Endereçamento Postal">
+            <br>
+            <br>
+            <label for="">Data de Nascimeto:</label>
+            <br>
+            <input type="number" placeholder="Digite sua data de nascimeto">
+
 
         </form>
     </div>
