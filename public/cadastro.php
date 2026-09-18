2666d3f3f16d5ab34a27590f05ac45fff866070b
RoederArt <arthur_roeder@estudante.sesisenai.org.br>
2026-09-18 11:56:57 -0300

    up - css do cadastro completo

--- public/cadastro.php
+++ public/cadastro.php
@@ -1,69 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/style.css">
     <title>Cadastro</title>
 </head>
+
 <body>
 
-<header>
-    <nav>
-        <a href="">Login</a>
-        <a href="">Cadastro</a>
-        <a href="">Panorama geral</a>
-    </nav>
-</header>
-    
-<main>
-    <div>
-        <form>
-            <h1>CADASTRO</h1>
-            <label for="">Nome completo:</label>
-            <br>
-            <input type="text" placeholder="Digite seu nome completo"> 
-            <br>
-            <br>
-            <label for="">E-mail:</label>
-            <br>
-            <input type="email" placeholder="Digite seu E-mail"> 
-            <br>
-            <br>
-            <label for="">CPF:</label>
-            <br>
-            <input type="number" placeholder="Digite seu CPF"> 
-            <br>
-            <br>
-            <label for="">Área de atuação:</label>
-            <br>
-            <input type="email" placeholder="Digite a área em que atua">
-            <br>
-            <br>
-            <label for="">Endereço:</label>
-            <br>
-            <input type="text" placeholder="Digite seu endereço">
-            <br>
-            <br>
-            <label for="">Cidade:</label>
-            <br>
-            <input type="text" placeholder="Digite sua cidade">
-            <br>
-            <br>
-            <label for="">CEP:</label>
-            <br>
-            <input type="number" placeholder="Digite seu Código de Endereçamento Postal">
-            <br>
-            <br>
-            <label for="">Data de Nascimeto:</label>
-            <br>
-            <input type="date" placeholder="Digite sua data de nascimeto">
-            
-        </form>
-    </div>
-</main>
+    <header class="cadastro-header">
+        <nav>
+            <img src="../assets/logo.png" alt="Logo do site">
+        </nav>
+    </header>
 
-<footer></footer>
+    <main class="cadastro-pagina">
+        <section class="cadastro-formulario">
+                <form>
+                    <h2>CADASTRO</h2>
+                    <div class="cadastro-campo">
+                        <label for="">Nome completo:</label>
+                        <br>
+                        <input type="text" placeholder="Digite seu nome completo">
+                    </div>
+                    <br>
+                    <br>
+                    <div class="cadastro-campo">
+                        <label for="">E-mail:</label>
+                        <br>
+                        <input type="email" placeholder="Digite seu E-mail">
+                    </div>
+                    <br>
+                    <br>
+                    <div class="cadastro-campo">
+                        <label for="">CPF:</label>
+                        <br>
+                        <input type="number" placeholder="Digite seu CPF">
+                    </div>
+                    <br>
+                    <br>
+                    <div class="cadastro-campo">
+                        <label for="">Área de atuação:</label>
+                        <br>
+                        <input type="email" placeholder="Digite a área em que atua">
+                    </div>
+                    <br>
+                    <br>
+                    <div class="cadastro-campo">
+                        <label for="">Endereço:</label>
+                        <br>
+                        <input type="text" placeholder="Digite seu endereço">
+                    </div>
+                    <br>
+                    <br>
+                    <div class="cadastro-campo">
+                        <label for="">Cidade:</label>
+                        <br>
+                        <input type="text" placeholder="Digite sua cidade">
+                    </div>
+                    <br>
+                    <br>
+                    <div class="cadastro-campo">
+                        <label for="">CEP:</label>
+                        <br>
+                        <input type="number" placeholder="Digite seu Código de Endereçamento Postal">
+                    </div>
+                    <br>
+                    <br>
+                    <div class="cadastro-campo">
+                        <label for="">Data de Nascimeto:</label>
+                        <br>
+                        <input type="date" placeholder="Digite sua data de nascimeto">
+                    <br>
+                    <br>
+                    <button type="submit">Cadastrar</button>
+
+                    </div>
+                </form>
+        </section>
+    </main>
+
+     <img class="fixa" src="../assets/detalhe_pagina.png" alt="Detalhe da página">
+
+    <footer></footer>
 
 </body>
+
 </html>
