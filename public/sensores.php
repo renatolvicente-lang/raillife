f77aae29d7902fd998df580c47ad5dddca5ff54f
Mateus de Lima dos Santos <mateus_lima-santos@estudante.sesisenai.org.br>
2026-09-21 12:08:56 -0300

    add - Página "Sensores" criada

---
+++ public/sensores.php
@@ -0,0 +1,58 @@
+<?php?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <link rel="stylesheet" href="../assets/style.css">
+    <title>Sensores</title>
+</head>
+<body>
+    <header class="hd_apresentacao">
+            <div>
+                <img src="../assets/logo.png" alt="" class="img_apresentacao">
+            </div>
+            <nav class="nav_apresentacao">
+                <a href="#">Login</a>
+                <a href="#">Cadastro</a>
+            </nav>
+    </header>
+
+    <main class="mn_apresentacao">
+        <div class="card_lista">
+
+            <h2>Lista de Sensores:</h2>
+                <div class="lista">
+                    <div class="item_lista">
+                        <h3>Sensor de Temperatura A-2</h3>
+                        <p>ID: 001</p>
+                    </div>
+                    <div class="item_lista">
+                        <h3>Sensor de Umidade B-1</h3>
+                        <p>ID: 002</p>
+                    </div>
+                    <div class="item_lista">
+                        <h3>Sensor de Pressão C-3</h3>
+                        <p>ID: 003</p>
+                    </div>
+                    <div class="item_lista">   
+                        <h3>Sensor de Luz D-4</h3>
+                        <p>ID: 004</p>
+                    </div>
+                    <div class="item_lista">
+                        <h3>Sensor de Movimento E-5</h3>
+                        <p>ID: 005</p>
+                    </div>
+                </div>
+            </div>
+
+    </main>
+
+    <img src="../assets/detalhe_pagina.png" alt="#" class="fixa">
+    <footer class="ft_apresentacao">
+        <p>©Direitos Reservados</p>
+    </footer>
+</body>
+</html>
