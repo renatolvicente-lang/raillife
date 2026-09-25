25c65b59489bdd987ffd788d7d79cb5908a506db
GustavoFenske <gustavo_fenske@estudante.sesisenai.org.br>
2026-09-25 11:48:35 -0300

    fix - simplify CEP validation logic in cadastro.php

--- public/cadastro.php
+++ public/cadastro.php
@@ -17,19 +17,15 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     }
 
     $cep_limpo = preg_replace('/\D/', '', $CEP);
-    $quantidade_digitos_cep = strlen($cep_limpo);
-
-    if ($quantidade_digitos_cep > 8) {
-        exit;
-    } elseif ($quantidade_digitos_cep < 8) {
-        exit;
+    if (strlen($cep_limpo) !== 8) {
+        exit("CEP inválido");
     }
 
     $sql = "INSERT INTO usuarios (nome, email, senha, CPF, endereco, cidade, CEP, data_nascimento) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
     $comando = $conn->prepare($sql);
 
-    $comando->bind_param("sssssss",$nome, $email, $senha, $CPF, $endereco, $cidade, $CEP, $data_nascimento);
+    $comando->bind_param("ssssssss",$nome, $email, $senha, $CPF, $endereco, $cidade, $CEP, $data_nascimento);
     $comando->execute();
 
     header("Location: ../public/login.php");
